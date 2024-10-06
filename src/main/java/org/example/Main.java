b91c0f5a1ee9ab95fe244e17befadec36077bd42
HugoSanchezGallego <hsancgal@myuax.com>
2024-10-06 19:21:36 +0200

    Enlazarlo al docker (intentandolo)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,34 +1,30 @@
 package org.example;
 
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        String csvFile = "src/main/java/csv/IQ_level_limpio.csv";
-        CSVReader reader = null;
-        try {
-            reader = new CSVReader(new FileReader(csvFile));
-            String[] line;
-            while ((line = reader.readNext()) != null) {
-                for (String cell : line) {
-                    System.out.print(cell + " ");
-                }
-                System.out.println();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (CsvValidationException e) {
-            e.printStackTrace();
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+        String jdbcUrl = "jdbc:mysql://localhost:3308/my-app?serverTimezone=UTC";
+        String username = "root";
+        String password = "P4ssword!";
+
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
+            System.out.println("Connected to the database!");
+
+            String query = "SELECT 1";
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(query)) {
+
+                while (resultSet.next()) {
+                    int result = resultSet.getInt(1);
+                    System.out.println("Query result: " + result);
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
